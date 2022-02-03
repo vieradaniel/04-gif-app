0ab7e28520d1c65ac10aa63cdf4ad9db987c5d6b
vieradaniel <dariennx@hotmail.com>
2022-02-03 08:23:53 -0300

    I finished testing gifGridItem component using .prop()

--- src/tests/GifGridItem.test.js
+++ src/tests/GifGridItem.test.js
@@ -5,14 +5,42 @@ import { shallow } from 'enzyme';
 import { GifGridItem } from '../components/GifGridItem';
 
 
+
 describe('pruebas en <GifGridItem/>',()=>{
 
-
+    const url="http://localhost/algo.jpg";
+    const title="un titulo";
+    const wrapper = shallow(<GifGridItem title={title} url={url}/>);
+    
     test('debe de mostrar <GifGridItem/> correctamente',()=>{
 
-        const wrapper = shallow(<GifGridItem/>);-
+
         expect(wrapper).toMatchSnapshot();
 
+
+    })
+
+    test('debe de tener un párrafo con el title',()=>{
+
+        const p = wrapper.find('p');
+
+        expect(p.text().trim()).toBe(title);
+
+    })
+
+    test('debe tener la imagen y la url de los props',()=>{
+
+        const img = wrapper.find('img');
+        expect(img.prop('src')).toBe(url) ;
+        expect(img.prop('alt')).toBe(title) ;
+
+    });
+
+    test('debe de tener animate__fadeIn',()=>{
+
+        const div = wrapper.find('div');
+        
+        expect(div.prop('className').includes('animate__fadeIn')).toBe(true);
 
     })
 
